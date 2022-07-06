09e1306ff9c648532cd3bb73b8d0b4d9ce8f09c9
Mary Kaki <justonly4testing@gmail.com>
2022-07-06 12:52:37 +0300

    I can now loop through all pages of ships without hard coding; this is not perfect but it works for now; I will improve the error in the loop later.

--- main.py
+++ main.py
@@ -5,24 +5,64 @@ from pprint import pprint
 
 client = pymongo.MongoClient()
 
-db = client['starwars']
+# db = client['starwars']
+#
+# luke = db.characters.find_one({"name": "Luke Skywalker"})
 
-luke = db.characters.find_one({"name": "Luke Skywalker"})
 
-
-ships = requests.get("https://swapi.dev/api/starships/?page=2")
+ships = requests.get("https://swapi.dev/api/starships/?page=1")
 
 ships_data = ships.json()
 
-for ship in ships_data["results"]:
-    print("SHIP ####################################")
-    pprint(ship["name"])
-    for pilot in ship["pilots"]:
-        print("PILOT ####################################")
-        # print(pilot)
-        pilot_data = requests.get(pilot)
-        pd = pilot_data.json()
-        pprint(pd["name"])
+# pprint(ships_data)
+page = 1
+ship_number = 0
+
+while ships_data["count"] :
+    # pprint(ships_data)
+
+    page += 1
+
+    ships = requests.get(f"https://swapi.dev/api/starships/?page={page}")
+
+
+    ships_data = ships.json()
+    print("##########################################################################################")
+    print(f"PAGE {page}")
+    # pprint(ships_data)
+    ship_number = 0
+    for ship in ships_data["results"]:
+        print("SHIP ####################################")
+        pprint(ship["name"])
+        ship_number += 1
+        for pilot in ship["pilots"]:
+            print("PILOT ####################################")
+            # print(pilot)
+            pilot_data = requests.get(pilot)
+            pd = pilot_data.json()
+            pprint(pd["name"])
+    print(f"There are {ship_number} ships")
+
+
+
+
+
+
+
+
+
+# ship_number = 0
+# for ship in ships_data["results"]:
+#     print("SHIP ####################################")
+#     pprint(ship["name"])
+#     ship_number += 1
+#     for pilot in ship["pilots"]:
+#         print("PILOT ####################################")
+#         # print(pilot)
+#         pilot_data = requests.get(pilot)
+#         pd = pilot_data.json()
+#         pprint(pd["name"])
+# print(f"There are {ship_number} ships")
 
 
 
