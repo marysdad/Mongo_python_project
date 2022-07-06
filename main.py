c138bc9ad2e3188453b61d0545aa989acde74755
Mary Kaki <justonly4testing@gmail.com>
2022-07-06 16:43:47 +0300

    Now the code is full commented; the last line is not working as intended.

--- main.py
+++ main.py
@@ -18,16 +18,17 @@ ships_data = ships.json()
 page = 0
 ship_number = 0
 
+
+# this loops through all the ship data and only ends if there is no data left
 doing = True
 while doing != False :
     # pprint(ships_data)
 
+    # this tracts and at the end show how many pages were scraped
     page += 1
 
     ships = requests.get(f"https://swapi.dev/api/starships/?page={page}")
-    if ships_data["results"] == "None":
-        # doing = False
-        break
+
 
     ships_data = ships.json()
     print("##########################################################################################")
@@ -35,39 +36,53 @@ while doing != False :
     # pprint(ships_data)
 
 
-
+    # this loops through the data of each ship
     for ship in ships_data["results"]:
 
         print("SHIP ####################################")
         pprint(ship["name"])
-        ship_number += 1
-        pilots_id_list = []
+        ship_number += 1 # this tracks how many ships have been read
+        pilots_id_list = [] # this is list for pilot ids to replace the list of pilot APIs
+
+        # loops through each pilot API if there is more than one
         for pilot in ship["pilots"]:
             print("PILOT ####################################")
             # print(pilot)
+
+            # This is make pilot API call to get the pilot data and store in pd
             pilot_data = requests.get(pilot)
             pd = pilot_data.json()
 
-            pilot_name = pd["name"]
+
+            pilot_name = pd["name"] # extracts pilot name
+
+            # this gets pilot id from DB that matches pilot name from pilot API
             pilot_id = db.characters.find_one({"name": pilot_name},{"_id"})
+
+
             print("         pilot ", pilot_name, " ", pilot_id )
             # ship["pilots"] = ship["pilots":[].append(pilot_id)]
 
+            # if ship does have pilots, add pilot ID to the list of pilot IDs for this ship
             if pilot:
                 pilots_id_list.append(pilot_id)
-        ship["pilots"] = pilots_id_list
+
+        ship["pilots"] = pilots_id_list # this replaces the list of pilot API with the list of pilot ID
 
 
 
     print(f"There are {ship_number} ships")
 
 
-    pprint(ships_data)
+
+
+    # this ends the loop if there is not more data left
     if  ships_data["next"] == None :
         break
 
 
-
+# this displaces ship data set with pilot API replace with the pilot IDs
+pprint(ships_data)
 
 
 
